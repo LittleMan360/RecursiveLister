78a92a33d9e3d59ec304c86488c7bb728fc6f791
dalto <dalton.m.towe@gmail.com>
2024-04-21 22:23:35 -0400

    created gui no actions yet just visual

--- src/RecursiveLister.java
+++ src/RecursiveLister.java
@@ -1,5 +1,37 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+public class RecursiveLister extends JFrame {
+    private JButton startButton = new JButton("Start");
+    private JButton quitButton = new JButton("Quit");
+    private JTextArea textArea = new JTextArea();
+    private JScrollPane scrollPane = new JScrollPane(textArea);
+
+    public RecursiveLister() {
+        setLayout(new BorderLayout());
+        add(new JLabel("Recursive File Lister"), BorderLayout.NORTH);
+        add(scrollPane, BorderLayout.CENTER);
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.add(startButton);
+        buttonPanel.add(quitButton);
+        add(buttonPanel, BorderLayout.SOUTH);
+
+        startButton.addActionListener((ActionEvent e) -> {
+            JFileChooser fileChooser = new JFileChooser();
+            fileChooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+            int returnValue = fileChooser.showOpenDialog(null);
+            if (returnValue == JFileChooser.APPROVE_OPTION) {
+                File selectedFile = fileChooser.getSelectedFile();
+                listFiles(selectedFile);
+            }
+        });
+
+        quitButton.addActionListener((ActionEvent e) -> {
+            System.exit(0);
+        });
     }
+
+
 }
